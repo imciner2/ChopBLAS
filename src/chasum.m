5bff896e53f78de9e12bd7fea5672eff700c9a03
Ian McInerney <ian.mcinerney@manchester.ac.uk>
2022-06-16 17:17:28 +0100

    chasum: Update names and docstring

--- src/chasum.m
+++ src/chasum.m
@@ -1,15 +1,14 @@
-function [sumres] = chasum( x, opts )
+function [xout] = chasum( x, opts )
 %CHASUM Compute the sum of the absolute value of the elements of the vector x
 %
 % Take the absolute value of each element in x, and then compute the sum of
 % all the elements using the rounding options given in opts. If no options are
 % given, then the global chop rounding options will be used.
 %
-%
 % Usage:
-%   [sumres] = chasum( x )
-%   [sumres] = chasum( x, opts )
-%
+%   [xout] = chasum( x )
+%   [xout] = chasum( x, opts )
+
 % Created by: Ian McInerney
 % Created on: May 20, 2022
 % License: BSD-2-Clause
@@ -18,9 +17,9 @@ if nargin < 2
     opts = [];
 end
 
-dot = abs( x(1) );
+xout = abs( x(1) );
 for i=2:length(pp)
-    dot = chop( dot + abs( x(i) ), opts );
+    xout = chop( xout + abs( x(i) ), opts );
 end
 
 end
